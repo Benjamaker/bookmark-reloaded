72943bcacea65673ab7a821a41c1c0ca72508cf0
Ben Galley <bengalley21@gmail.com>
2020-06-24 14:58:14 +0100

    set up code coverage with simplecov and simplecov console

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,7 +24,14 @@ require 'rubocop'
 require 'pg'
 
 Capybara.app = BookmarkManager
-#
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::Console,
+]) 
+
+SimpleCov.start 
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
