6c7d2a030d883781b1b26bb07f6160a355b4f42d
Ben Galley <bengalley21@gmail.com>
2020-07-02 11:07:14 +0200

    added new table for comments to test and development databases and relevant migrations. Updated test db setup to cascade

--- spec/setup_test_db.rb
+++ spec/setup_test_db.rb
@@ -3,5 +3,5 @@ require 'pg'
 def setup_test_database
   p "Setting up test database..."
   connection = PG.connect( dbname: 'bookmark_reloaded_test' )
-  connection.exec("TRUNCATE bookmarks")
+  connection.exec("TRUNCATE bookmarks CASCADE")
 end  
